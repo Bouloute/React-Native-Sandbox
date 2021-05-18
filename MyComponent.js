62a03b5caf8eb8a038c7a5854cd3b112f4257de5
Bouloute <annaick1995@gmail.com>
2021-05-17 21:15:49 -0400

    first commit

---
+++ MyComponent.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class MyComponent extends Component {
+    render() {
+        return (
+            <div>
+                HELLOOOOOO Name is Bipo
+            </div>
+        )
+    }
+}
